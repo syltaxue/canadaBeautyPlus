233684c9e1bca5a197681a2c934714bc037a15cb
syltaxue <syltaxue@hotmail.com>
2016-03-13 00:53:01 -0800

    update setup

--- app/src/js/router.js
+++ app/src/js/router.js
@@ -4,10 +4,10 @@ var render = require('react-dom').render;
 var Router = require('react-router').Router;
 var Route = require('react-router').Route;
 // This library is for listening all the events for material-ui
-// var injectTapEventPlugin = require("react-tap-event-plugin");
+var injectTapEventPlugin = require("react-tap-event-plugin");
 var Main = require('./home/main');
 var ProductPage = require('./productPage/main');
-
+injectTapEventPlugin();
 render((
 	<Router>
 		<Route path="/" component={Main}>
